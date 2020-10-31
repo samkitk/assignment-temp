1dd3061d9bbb81313c7b8eb7c5c6503dc27393e2
Malav Doshi <73582274+malav312@users.noreply.github.com>
2020-10-31 22:10:57 +0530

    New files

--- Heap.java
+++ Heap.java
@@ -1,20 +1,28 @@
+package heap;
+
 import java.util.ArrayList;
 
-public class Heap<Integer> {
-    protected ArrayList<Integer> array = new ArrayList<Integer>();
-    protected int heap_size;
+public class Heap<T> {
+	
+    
+	protected ArrayList<T> array = new ArrayList<T>();
+	protected int heap_size;
+	
+	public int Parent(int i) {
+		return i/2;		
+}
 
-    public int Parent(int i) {
-        return i/2;
-    }
 
-
-    public int Left(int i) {
+public int Left(int i) {
 	return 2*i;
-    }
+}
 
 
-    public int Right(int i) {
-	return (2*i) + 1;
-    }
+public int Right(int i) {
+	return 2*i+1;
+}
+
+	
+	
+	
 }
