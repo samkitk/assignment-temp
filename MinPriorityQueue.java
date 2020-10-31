b2736caab5af5078ab03d8ad7c74a6bf48a7189c
Malav Doshi <73582274+malav312@users.noreply.github.com>
2020-10-31 22:17:36 +0530

    Updated Files

--- MinPriorityQueue.java
+++ MinPriorityQueue.java
@@ -1,60 +1,51 @@
-
+package heap;
 
 import java.util.ArrayList;
 
 public class MinPriorityQueue extends MinHeap implements MinPriorityQueueIN<Integer> {
-        int flag = 0;
+
 	public MinPriorityQueue(ArrayList<Integer> arr) {
 		super(arr);
-		
 	}
 
 	@Override
 	public void Insert(Integer x) {
-		// IntegerODO Auto-generated method stub
-            array.set(heap_size - 1, java.lang.Integer.MAX_VALUE);
-            DecreaseKey(x, heap_size - 1);
-            heap_size++;
-            if(flag == 0) System.out.println("Successfully inserted value in min heap with all properties satisfied");
+            array.set(heap_size-1,java.lang.Integer.MIN_VALUE);
+            DecreaseKey(heap_size-1,x);	
+            heap_size=heap_size+1;
 	}
 
 	@Override
 	public Integer Minimum() {
-		return array.get(0);
+
+            return array.get(0);
 	}
 
 	@Override
 	public Integer ExtractMin() {
-            Integer min = null;
-            if(heap_size-1 < 1) System.out.println("Heap Underflow!");
-            else
-            {
-                min = array.get(0);
-                array.set(0, array.get(heap_size-1));
-                heap_size--;
-                MinHeapify(array, 0);
+           if(heap_size<1){
+               System.out.println("heap underflow");
+               return null;
             }
-            return min;
+            Integer min=array.get(0);
+            array.set(0,array.get(heap_size-1));
+            heap_size=heap_size-1;
+            MinHeapify(array,0) ;                   
+		return array.get(0);
 	}
 
 	@Override
 	public void DecreaseKey(int x, Integer k) {
-            if(x > array.get(k))
-            {
-                System.out.println("New Key is larger than current key");
-                flag = 1;
+            if(k<array.get(x)){
+                System.out.println("new key is larger than current key");
             }
-            else
-            {
-                array.set(k, x);
-                while(k > 1 && array.get(Parent(k)) < array.get(k))
-                {
-                    Integer temp = array.get(k);
-                    array.set(k, array.get(Parent(k)));
-                    array.set(Parent(k), temp);
-                    k = Parent(k);
-                }
+            array.set(x, k);
+            while((x>1)&& array.get(Parent(x))>array.get((x))){
+                int temp;
+                temp=array.get(x);
+                array.set(x,array.get(Parent(x)));
+                array.set(Parent(x), temp);
+                x=Parent(x);
             }
 	}
-
 }
