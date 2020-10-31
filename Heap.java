8ea838dc8ca064279e4e34ca0eedf1e55368be2b
Malav Doshi <73582274+malav312@users.noreply.github.com>
2020-10-31 22:00:59 +0530

    Text

--- Heap.java
+++ Heap.java
@@ -5,19 +5,16 @@ public class Heap<Integer> {
     protected int heap_size;
 
     public int Parent(int i) {
-        //your code here and change return
         return i/2;
     }
 
 
     public int Left(int i) {
-	//your code here and change return
 	return 2*i;
     }
 
 
     public int Right(int i) {
-	//your code here and change return
 	return (2*i) + 1;
     }
 }
